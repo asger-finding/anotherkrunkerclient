399d8129c7bcffb16dfc87b1974147228f5e3737
Commander <asger.finding@gmail.com>
2022-04-11 23:23:04 +0200

    Refactor proxy fix and fix https://stackoverflow.com/a/44854201/11452298 (pt. 2)

--- src/window/game-pre.ts
+++ src/window/game-pre.ts
@@ -30,11 +30,6 @@ import { MapExport } from '../krunker';
 	// Proxy JSON.parse
 	const proxy = JSON.parse;
 	JSON.parse = function(...args) {
-		/**
-		 * See issue: https://stackoverflow.com/a/6599105/11452298
-		 * This is a hack and will not work if it is not tested with the /\{\s+\[native code\]/ expression
-		 */
-		// { [native code] }
 		const parsed: MapExport = proxy.apply(this, args);
 
 		// Check if the parsed object is a map.
@@ -49,11 +44,31 @@ import { MapExport } from '../krunker';
 		return parsed;
 	};
 
-	/** See issue: https://stackoverflow.com/a/44854201/11452298 */
-	JSON.parse.toString = String.bind(0, 'function parse() { [native code] }');
+	/** Addresses https://stackoverflow.com/a/44854201/11452298 and https://stackoverflow.com/a/28121768/11452298 */
 
-	/** See issue: https://stackoverflow.com/a/28121768/11452298 */
-	delete JSON.parse.prototype.constructor;
+	// Array of native function names
+	const proxyDict = { 'JSON.parse': 'function parse() { [native code] }' };
+
+	for (const [ key, value ] of Object.entries(proxyDict)) {
+		// eslint-disable-next-line no-eval
+		const func = eval(key);
+
+		func.toString = String.bind(null, value);
+		delete func.prototype.constructor;
+	}
+
+	Function.prototype.toString.call = new Proxy(Function.prototype.toString.call, {
+		apply(target, thisArg, args) {
+			const [func] = args;
+			const result = func.toString();
+
+			// If we have it in our proxy dictionary, return the .toString() result as a native .
+			if (Object.values(proxyDict).includes(result)) return result;
+
+			// Call the original function and return the result.
+			return target.call(thisArg, func);
+		}
+	});
 
 	// Show the client exit button
 	document.addEventListener('DOMContentLoaded', () => {
