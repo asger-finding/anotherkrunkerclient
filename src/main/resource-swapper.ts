a8054459828b2bdaa2cd534e9d35f7ef6ab3b7f3
Asger Finding <asger.finding@gmail.com>
2022-07-14 00:01:51 +0200

    refactor: resource swapper performance improvement

--- src/main/resource-swapper.ts
+++ src/main/resource-swapper.ts
@@ -27,36 +27,46 @@ export default class {
 		// If the target directory doesn't exist, create it.
 		if (!existsSync(global.resourceswapProtocolSource)) mkdirSync(global.resourceswapProtocolSource, { recursive: true });
 
-		this.recursiveSwap('');
+		this.readSwapDirectory();
 		if (this.urls.length) {
 			this.window.webContents.session.webRequest.onBeforeRequest({ urls: this.urls }, (details, callback) => {
 				const path = new URL(details.url).pathname;
+
+				// Redirect to the local resource.
 				callback({ redirectURL: `${ CLIENT_NAME }:/${ path.startsWith('/assets/') ? path.substring(7) : path }` });
 			});
 		}
 
 		// Fix CORS problem with browserfps.com.
-		this.window.webContents.session.webRequest.onHeadersReceived((details, callback) => {
-			for (const key in details.responseHeaders) {
+		this.window.webContents.session.webRequest.onHeadersReceived(({ responseHeaders }, callback) => {
+			for (const key in responseHeaders) {
 				const lowercase = key.toLowerCase();
 
 				// If the credentials mode is 'include', callback normally or the request will error with CORS.
-				if (lowercase === 'access-control-allow-credentials' && details.responseHeaders[key][0] === 'true') return callback(details.responseHeaders);
+				if (lowercase === 'access-control-allow-credentials' && responseHeaders[key][0] === 'true') return callback(responseHeaders);
 
 				// Response headers may have varying letter casing, so we need to check in lowercase.
 				if (lowercase === 'access-control-allow-origin') {
-					delete details.responseHeaders[key];
+					delete responseHeaders[key];
 					break;
 				}
 			}
 
 			return callback({
 				responseHeaders: {
-					...details.responseHeaders,
+					...responseHeaders,
 					'access-control-allow-origin': ['*']
 				}
 			});
 		});
+	}
+
+	public readSwapDirectory(): void {
+		// Clear the list of URLs.
+		this.urls = [];
+
+		// Read over 
+		this.recursiveSwap('');
 	}
 
 	/**
@@ -82,7 +92,7 @@ export default class {
 						: [
 							...tests,
 							`*://comp.${ TARGET_GAME_DOMAIN }${ name }?*`,
-							`*://comp.${ TARGET_GAME_DOMAIN }assets/${ name }?*`
+							`*://comp.${ TARGET_GAME_DOMAIN }/assets/${ name }?*`
 						]
 					));
 				}
